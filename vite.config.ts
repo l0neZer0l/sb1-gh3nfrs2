4b3ecc7ed1cee4af60b340a9de1acc0bcdc52f03
l0neZer0l <adembensalem8@gmail.com>
2025-03-07 15:51:22 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
+    port:3000,
     proxy: {
       '/auth': 'https://sb1-gh3nfrs2-d99r4k29u-l0nezer0ls-projects.vercel.app',
       '/api': 'https://sb1-gh3nfrs2-d99r4k29u-l0nezer0ls-projects.vercel.app'
